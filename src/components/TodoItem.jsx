2cfae04b924d451923b3113c544cb763501a9700
Vladyslav Konotovskyi <konotovsky@gmail.com>
2025-08-04 18:10:18 +0300

    Styled TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,10 +12,11 @@ export function TodoItem({ itemValue, itemIsDone, cardId, itemId }) {
   };
 
   return (
-    <li>
-      <label>
+    <li className="border-b-1 border-gray-100 py-2">
+      <label className="flex cursor-pointer items-center gap-2 text-sm break-words hyphens-auto">
         <input
           type="checkbox"
+          className="h-5 w-5 shrink-0 cursor-pointer appearance-none rounded-full border-2 border-red-200 checked:bg-red-400"
           checked={itemIsDone}
           onChange={() => handleToggleChange(cardId, itemId)}
         ></input>
